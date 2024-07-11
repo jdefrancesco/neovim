ea7ba52e3ab19ba1d5de8a73e6ef7591e90e4854
jdefr89 <jdefr89@gmail.com>
2024-07-11 07:11:08 -0400

    Turned on tabbing for CoC

--- init.lua
+++ init.lua
@@ -95,8 +95,8 @@ vim.cmd([[
 ]])
 
 -- Copilot settings
-vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-Space>", 'copilot#Accept("\\<CR>")', { silent = true, expr = true, script = true })
+vim.g.copilot_no_tab_map = false
+-- vim.api.nvim_set_keymap("i", "<C-Space>", 'copilot#Accept("\\<CR>")', { silent = true, expr = true, script = true })
 
 
 -- General settings
